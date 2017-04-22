9f714861daec03ad1c5249604690bd3ca7140ab2
huihuizhao <zhaohuihui@outlook.com>
2017-04-22 08:03:21 +0800

    level9

--- QTMTest/Form1.cs
+++ QTMTest/Form1.cs
@@ -21,22 +21,22 @@ namespace QTMTest
         private void button1_Click(object sender, EventArgs e)
         {
 
-            FileStream fs = new FileStream(@"D:\QTMTest01.txt", FileMode.Create);
+            FileStream fs = new FileStream(@"D:\QTMTest0903.txt", FileMode.Create);
             StreamWriter sw = new StreamWriter(fs);
 
 
 
             //level 9
-            //float  P = Math.Pow(4,9);
-            //float I=512;
+            float P = (float)Math.Pow(4, 9);
+            float I = 512;
 
             //level 3
             //float P = Math.Pow(4, 3);
             //float I = 56;
 
             //level 3
-            float P =(float ) Math.Pow(4, 2);//一个oct 的单元数
-            float I = 4;//行数
+            //float P = (float)Math.Pow(4, 2);//一个oct 的单元数
+            //float I = 4;//行数
 
             //for (int N=0;N<262144*8;N++)
             for (int N = 0; N < P * 8; N++)
@@ -68,12 +68,16 @@ namespace QTMTest
                 {
                     lon = (((float)1 / 3) * ((90 / i) * A + (90 / i) * B + (90 / (i + 1)) * B));
                     lat = ((float)1 / 3) * (C + 2 * D);
-                
+
                 }
 
 
             if (0 <= N&&N < P * 1)
             {
+                if (double.IsNaN(lon))
+                {
+                    lon = 0;
+                }
                 Lon=lon;
                 Lat=lat;
 
@@ -81,13 +85,20 @@ namespace QTMTest
             }
             else if (P * 1 <= N && N < P * 2)
             {
-
+                if (double.IsNaN(lon))
+                {
+                    lon = 90;
+                }
                 Lon=lon+90;
                 Lat=lat;
 
             }
             else if (P * 2 <= N && N < P * 3)
             {
+                if (double.IsNaN(lon))
+                {
+                    lon = -180;
+                }
 
                                 Lon=lon-180;
                 Lat=lat;
@@ -95,6 +106,10 @@ namespace QTMTest
             }
             else if (P * 3 <= N && N < P * 4)
             {
+                if (double.IsNaN(lon))
+                {
+                    lon = -90;
+                }
                 Lon=lon-90;
                 Lat=lat;
 
@@ -102,6 +117,10 @@ namespace QTMTest
             }
             else if (P * 4 <= N && N < P * 5)
             {
+                //if (double.IsNaN(lon))
+                //{
+                //    lon = 0;
+                //}
 
                                 Lon=90-lon;
                 Lat=-lat;
@@ -109,13 +128,20 @@ namespace QTMTest
             }
             else if (P * 5 <= N && N < P * 6)
             {
-
+                //if (double.IsNaN(lon))
+                //{
+                //    lon = 90;
+                //}
                                                 Lon=180-lon;
                 Lat=-lat;
 
             }
             else if (P * 6 <= N && N < P *7)
             {
+                //if (double.IsNaN(lon))
+                //{
+                //    lon = -180;
+                //}
                                                 Lon=-90-lon;
                 Lat=-lat;
 
@@ -123,6 +149,10 @@ namespace QTMTest
             }
             else if (P * 7 <= N && N < P * 8)
             {
+                //if (double.IsNaN(lon))
+                //{
+                //    lon = -90;
+                //}
                                                 Lon=-lon;
                 Lat=-lat;
 
@@ -134,7 +164,7 @@ namespace QTMTest
 
             //开始写入
             //sw.WriteLine(N.ToString() + " " + Lon.ToString() + " " + Lat.ToString());
-            sw.WriteLine(N.ToString() + "," + Lon.ToString() + "," + Lat.ToString());
+            sw.WriteLine(N.ToString() + " " + Lon.ToString() + " " + Lat.ToString() + " ");
 
 
             }
